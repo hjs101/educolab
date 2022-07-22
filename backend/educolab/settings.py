042ca243da65a20fe44224719e0e3c967d2b4fc7
홍찬기 <sal0203@naver.com>
2022-07-22 21:41:08 +0900

    Feat : 프론트와 연결 가능하게 cors 추가

--- backend/educolab/settings.py
+++ backend/educolab/settings.py
@@ -34,9 +34,10 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts',
     'drf_yasg',
+    'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
-    'rest_framework',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +126,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.UserInfo'
+
+# 우선 모두 연결해 놓았습니다.
+CORS_ALLOW_ALL_ORIGINS = True
