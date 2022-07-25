51dde4733381cdbca0b3f4f7e4971bba7e296e9d
h5282001 <h5282001@naver.com>
2022-07-25 14:04:12 +0900

    Refactor : requirements.txt 파일 수정 및 url, views 수정사항 적용
    커밋 오류 수정 및 merge 수정사항 반영

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -16,16 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
 )
 from .serializers import MyTokenObtainPairView
-
+##aaadd
 urlpatterns = [
-    # path('login', views.LoginView.as_view(), name = "LoginView"),
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 	path('login/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
