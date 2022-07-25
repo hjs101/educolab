719770f05081632b312ce1daf54f0b78b5e7ec84
Ubuntu <ubuntu@ip-172-26-12-171.ap-northeast-2.compute.internal>
2022-07-25 16:58:39 +0900

    x

--- backend/my_settings.py
+++ backend/my_settings.py
@@ -3,8 +3,8 @@ DATABASES = {
 'ENGINE': 'django.db.backends.mysql',
 'NAME': 'educolab',
 'USER': 'educolab',
-'PASSWORD': 'educolab',
-"HOST": '13.125.213.119',
+'PASSWORD': 'c102_edu',
+"HOST": '127.0.0.1',
 "PORT": '3306',
 }
 }
