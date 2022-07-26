834c26a8bf36913b6b3857c581023dc411172d6a
h5282001 <h5282001@naver.com>
2022-07-26 18:32:29 +0900

    Fix : mySettings.py 수정

--- backend/my_settings.py
+++ backend/my_settings.py
@@ -3,10 +3,8 @@ DATABASES = {
 'ENGINE': 'django.db.backends.mysql',
 'NAME': 'educolab',
 'USER': 'educolab',
-'PASSWORD': 'educolab',
+'PASSWORD': 'c102_edu',
 "HOST": '13.125.213.119',
 "PORT": '3306',
+},
 }
-}
-
-MY_IMAGE_PATH = '/SSAFY/C102_IOT/S07P12C102/backend/image/'
