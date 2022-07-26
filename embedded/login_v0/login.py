37444e49334ee10f5167a4b0c625ca37e2a37f5b
Cookie-Rook <khlee025@gmail.com>
2022-07-27 06:39:54 +0900

    Feat : 로그인 화면(v1) 추가
    Resolves : #46

---
+++ embedded/login_v0/login.py
@@ -0,0 +1,24 @@
+import kivy
+
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.lang import Builder
+
+class loginApp(App):
+    def build(self):
+        Window.clearcolor = (242/255,245/255,247/255,1)
+        Window.size = (800,600)
+        return Builder.load_file('login.kv')
+    def forgetID(self):
+        print("Please make a function-forgetID") # ID 찾기 버튼 클릭시 실행되는 것을 여기에 작성해 주세요
+    def forgetPW(self):
+        print("Please make a function-forgetPW") # PW 찾기 버튼 클릭시 실행되는 것을 여기에 작성해 주세요
+    def loginbtn(self):
+        print("Please make a function-Login button") # login 버튼 클릭시 실행되는 것을 여기에 작성해 주세요
+
+if __name__=="__main__":
+    loginApp().run()
+
+
