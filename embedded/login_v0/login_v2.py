2b61321e95b267c1b64277fa9ff8b3abecb5164b
이국희 <khlee025>
2022-07-27 13:18:48 +0900

    Feat : 종료 기능 추가
    login_v0 : 화면 구성 및 버튼 클릭시 안내 문구 출력
    login_v1 : 종료 버튼 화면  추가
    login_v2 : DB 연결 및 창 종료 기능 추가
    
    See also : #46

--- embedded/login_v0/login_v2.py
+++ embedded/login_v0/login_v2.py
@@ -19,9 +19,11 @@ class loginApp(App):
         self.cur = self.db.cursor()
 
     def build(self):
+        Window.top=50
+        Window.left=10
         Window.clearcolor = (242/255,245/255,247/255,1)
-        Window.size = (800,600)
-        return Builder.load_file('login.kv')
+        Window.size = (1280,720)
+        return Builder.load_file('login_v2.kv')
         
     def forgetID(self):
         print("forgotID page") # ID 찾기 버튼 클릭시 실행되는 것을 여기에 작성해 주세요
@@ -29,10 +31,12 @@ class loginApp(App):
         print("forgotPW page") # PW 찾기 버튼 클릭시 실행되는 것을 여기에 작성해 주세요
     def loginbtn(self):
         print(self.cur.execute(""))
-    def on_stop(self):
+    def onStop(self):
         self.cur.close()
         self.db.close()
         Logger.critical('Good Bye')
+        ##window close
+        App.get_running_app().stop()
 
 if __name__=="__main__":
     loginApp().run()
