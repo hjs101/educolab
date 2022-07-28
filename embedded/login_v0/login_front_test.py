91aa23bbe7bf026024113f01b1aabefdf2cfd537
여동준 <tgc03042@naver.com>
2022-07-28 15:39:01 +0900

    Feat: kivy login page db 통신 확인

--- embedded/login_v0/login_front_test.py
+++ embedded/login_v0/login_front_test.py
@@ -1,46 +1,78 @@
-import kivy
 from kivy.app import App
 from kivy.core.window import Window
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
 from kivy.lang import Builder
-from kivy.logger import Logger
+from kivy.uix.screenmanager import Screen
+from myTextInput import limitedTextInput
 import mysql.connector
+import requests
+import hashlib
 
-class loginApp(App):
-    # def __init__(self, **kwargs):
-    #     super(loginApp, self).__init__(**kwargs)
-    #     self.db = mysql.connector.connect(
-    #         host='3.36.69.192',
-    #         user='educolab',
-    #         password='c102_edu',
-    #         database='educolab'
-    #     )
-    #     self.cur = self.db.cursor()
 
-    def build(self):
+class Login_Screen(Screen):
+    def __init__(self, **kwargs):
+        super(Login_Screen, self).__init__(**kwargs)
         Window.top=50
         Window.left=10
         Window.clearcolor = (242/255,245/255,247/255,1)
         Window.size = (1280,720)
         Window.borderless=True
-        return Builder.load_file('login_v3.kv')
-        
+        Builder.load_file('login_v3.kv')
+        self.db_init()
+
+    def db_init(self):
+        self.db = mysql.connector.connect(
+            host='13.125.213.119',
+            user='educolab',
+            password='educolab',
+            database='educolab'
+        )
+        self.cur = self.db.cursor()
+
     def forgetID(self):
-        print("forgotID page") # ID 찾기 버튼 클릭시 실행되는 것을 여기에 작성해 주세요
+        self.ID=self.ids.ID_input.text
+        if len(self.ID) == 0:
+            print("Nothing ID")
+        else:
+            print(self.ID) 
+
     def forgetPW(self):
-        print("forgotPW page") # PW 찾기 버튼 클릭시 실행되는 것을 여기에 작성해 주세요
+        self.PW=self.ids.PW_input.text       
+        if len(self.PW) == 0:
+            print("Nothing PW")
+        else:
+            print(self.PW) 
+
     def loginbtn(self):
-        # print(self.cur.execute(""))
-        print("main login page") # PW 찾기 버튼 클릭시 실행되는 것을 여기에 작성해 주세요
+        # 여기에는 여동준 부분
+        # email 대신 password, 창 넘기는 거까지 구현해야 할 듯
+        # password는 request module 써서 넘겨야 하는데 그건 테스트 해봐야 할 듯
+        self.query = ("SELECT username, email FROM accounts_userinfo "
+                        "where username=%s and email=%s")
+        self.ID = self.ids.ID_input.text
+        self.PW = self.ids.PW_input.text
+        self.cur.execute(self.query, (self.ID, self.PW))
+
+        if len(self.ID) == 0 or len(self.PW) == 0:
+            print("Nothing ID or PW")
+        else:
+            self.success_flag = False
+            for (username, email) in self.cur:
+                if username == self.ID and email == self.PW:
+                    self.success_flag = True
+                    break
+            if self.success_flag == True: print("성공!")
+            else: print("실패!")
+
     def onStop(self):
-        # self.cur.close()
-        # self.db.close()
-        # Logger.critical('Good Bye')
-        ##window close
+        self.db.close()
+        self.cur.close()
         App.get_running_app().stop()
 
+
+class login_test_App(App):
+    def build(self):
+        Builder.load_file('login_v3.kv')
+        return Login_Screen()
+
 if __name__=="__main__":
-    loginApp().run()
-
-
+    login_test_App().run()
