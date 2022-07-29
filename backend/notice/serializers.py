c5da375ad4a1d881746b8d3df1669242ce7fa666
h5282001 <h5282001@naver.com>
2022-07-29 23:28:59 +0900

    Feat : 공지사항 상세페이지 파일 표시, 수정기능(진행중)

--- backend/notice/serializers.py
+++ backend/notice/serializers.py
@@ -11,11 +11,16 @@ class NoticeMainSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Notice
         fields = ['pk','classification','title','created_at','views','teacher']
-
-class NoticeCreateSerializer(serializers.ModelSerializer):
+        
+class NoticeSerializer(serializers.ModelSerializer):
     teacher = TeacherNameSerializer(read_only=True)
     school = SchoolInfoSerializer(read_only=True)
     class Meta:
         model = models.Notice
         fields = '__all__'
 
+class FileSerializer(serializers.ModelSerializer):
+    atch_file = serializers.FileField(use_url=True)
+    class Meta:
+        model = models.Files
+        fields = '__all__'
--- backend/notice/views.py
+++ backend/notice/views.py
@@ -1,16 +1,11 @@
-from urllib import response
-from requests import request
+from django import views
+from pkg_resources import resource_isdir
 from rest_framework.decorators import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from accounts.serializers import TeacherNameSerializer
-from accounts.models import SchoolInfo,UserInfo
-from .serializers import NoticeMainSerializer, NoticeCreateSerializer
+from accounts.models import SchoolInfo
+from .serializers import NoticeMainSerializer, NoticeSerializer,FileSerializer
 from .models import Notice, Files
-from django.core.files.base import ContentFile
-from django.core.files.storage import default_storage
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
 import os, io
 
 
@@ -27,20 +22,99 @@ class NoticeMainView(APIView) :
         school = SchoolInfo.objects.get(code=schoolCode)
         notices = school.notice_school.all()
         # notices = Notice.objects.select_related('school').filter(school_id=schoolCode)
-        
+        print(notices)
         ## 3. 시리얼라이저 변환
         notice_serializer = NoticeMainSerializer(notices,many=True)
 
         ## 4. 가져온 목록 반환
         res = Response(notice_serializer.data)
 
-
         return res
 
 class NoticeCreateView(APIView):
     def post(self, req):
         # notice = Notice()
-        notice_serializer = NoticeCreateSerializer(data=req.data)
+        notice_serializer = NoticeSerializer(data=req.data)
+        if notice_serializer.is_valid(raise_exception=True):
+            notice_serializer.save(teacher=req.user, school=SchoolInfo.objects.get(code=req.data['school']))
+
+        files = req.FILES.getlist("files")
+        print(files)
+        notice = Notice.objects.all().order_by("-pk")
+        for file in files:
+            fp = Files.objects.create(notice=notice[0], atch_file=file, atch_file_name=file)
+            fp.save()
+        res = Response()
+        res.data = {
+            'message' : "success",
+        }
+        return res
+
+class NoticeDetailView(APIView):
+    def get(self, req):
+        ## 공지사항 번호 가져오기
+        notice_id = req.GET['notice_num']
+
+        ## 공지사항 번호로 공지사항 인스턴스 가져오기
+        notice = Notice.objects.get(pk=notice_id)
+
+
+        ## 조회수 1 올리기 
+        notice.views+=1
+        
+        notice.save()
+
+        files = notice.notice_file.all()
+        
+        
+        fileserializer = FileSerializer(files, many=True)
+        print(files)
+        
+        ## 공지사항 시리얼라이저 생성
+        notice_detail_serializer = NoticeSerializer(notice)
+        res = Response()
+        res.data = {
+            "notice" : notice_detail_serializer.data,
+            "files" : fileserializer.data,
+        }
+        if notice_detail_serializer.data['school']['code'] == req.user.school.code:
+            return res
+
+        
+        return Response({"message" : "잘못된 접근입니다."})
+
+class NoticeUpdateView(APIView):
+    def get(self, req):
+        ## 공지사항 번호 가져오기
+        notice_id = req.GET['notice_num']
+
+        ## 공지사항 번호로 공지사항 인스턴스 가져오기
+        notice = Notice.objects.get(pk=notice_id)
+        
+        notice.save()
+
+        files = notice.notice_file.all()
+        
+        
+        fileserializer = FileSerializer(files, many=True)
+        print(files)
+        
+        ## 공지사항 시리얼라이저 생성
+        notice_detail_serializer = NoticeSerializer(notice)
+        res = Response()
+        res.data = {
+            "notice" : notice_detail_serializer.data,
+            "files" : fileserializer.data,
+        }
+        if notice_detail_serializer.data['school']['code'] == req.user.school.code:
+            return res
+
+        
+        return Response({"message" : "잘못된 접근입니다."})
+
+    def post(self, req):
+        # notice = Notice()
+        notice_serializer = NoticeSerializer(data=req.data)
         if notice_serializer.is_valid(raise_exception=True):
             notice_serializer.save(teacher=req.user, school=SchoolInfo.objects.get(code=req.data['school']))
 
@@ -50,5 +124,8 @@ class NoticeCreateView(APIView):
         for file in files:
             fp = Files.objects.create(notice=notice[0], atch_file=file)
             fp.save()
-
-        return Response("success")
+        res = Response()
+        res.data = {
+            'message' : "success",
+        }
+        return res
