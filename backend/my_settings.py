81686e2b18f18fdb7a710a8c7b5d30a6211ac21b
wonkyoung <pubhan35@gmail.com>
2022-07-31 08:06:57 +0900

    Fix : 충돌 수정

--- backend/my_settings.py
+++ backend/my_settings.py
@@ -1,13 +1,12 @@
 DATABASES = {
 'default': {
-'ENGINE': 'django.db.backends.mysql',
-'NAME': 'educolab',
-'USER': 'educolab',
-'PASSWORD': 'educolab',
-"HOST": '13.125.213.119',
-"PORT": '3306',
-}
-<<<<<<< HEAD
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'educolab',
+    'USER': 'educolab',
+    'PASSWORD': 'educolab',
+    "HOST": '13.125.213.119',
+    "PORT": '3306',
+    }
 }
 
 MY_IMAGE_PATH = '/SSAFY/C102_IOT/S07P12C102/backend/image/'
@@ -15,6 +14,3 @@ MY_IMAGE_PATH = '/SSAFY/C102_IOT/S07P12C102/backend/image/'
 EMAIL_HOST_USER = 'educolabtest@gmail.com'
 
 EMAIL_HOST_PASSWORD = 'osbqrenzvuexioxx'
-=======
-}
->>>>>>> 4d6a26d19ac7385eb008cf669d3b078d95c0d850
