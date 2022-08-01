36a3f8f21f4e9d98277ce765109df2441d68b551
h5282001 <h5282001@naver.com>
2022-08-01 22:45:56 +0900

    Fix : 오타수정

--- front-end/src/store/modules/accounts.js
+++ front-end/src/store/modules/accounts.js
@@ -1,6 +1,6 @@
-import drf from "@/api/drf.js"
-import router from "@/router"
-import axios from "axios"
+import drf from "@/api/drf.js";
+import router from "@/router";
+import axios from "axios";
 
 export const accounts = {
   state() {
@@ -14,9 +14,9 @@ export const accounts = {
         grade: null,
         class_field: null,
         phone_number: null,
-        birthday: '2008-01-01',
+        birthday: "2008-01-01",
         email: null,
-        userflag : false,
+        userflag: false,
       },
       teacherInfo: {
         username: null,
@@ -26,103 +26,103 @@ export const accounts = {
         school: null, // code
         subject: null,
         phone_number: null,
-        birthday: '1972-01-01',
+        birthday: "1972-01-01",
         email: null,
-        userflag : true,
+        userflag: true,
       },
       userType: null,
-      access: localStorage.getItem('access') || '',
+      access: localStorage.getItem("access") || "",
       currentUser: {},
       authError: null,
-    }
+    };
   },
   getters: {
-    isLoggedIn: state => !!state.access, 
-    currentUser: state => state.currentUser,
-    authError: state => state.authError,
-    authHeader: state => ({ Authorization: `Token ${state.access}` }),
-    getUserType: state => state.userType,
-    getSubject: state => state.teacherInfo.subject,
+    isLoggedIn: (state) => !!state.access,
+    currentUser: (state) => state.currentUser,
+    authError: (state) => state.authError,
+    authHeader: (state) => ({ Authorization: `Token ${state.access}` }),
+    getUserType: (state) => state.userType,
+    getSubject: (state) => state.teacherInfo.subject,
   },
   mutations: {
-    SET_TOKEN: (state, access) => state.access = access,
-    SET_CURRENT_USER: (state, user) => state.currentUser = user,
-    SET_AUTH_ERROR: (state, error) => state.authError = error,
-    CHANGE_DATA(state,data) {
-      if (state.userType === 'student') {
+    SET_TOKEN: (state, access) => (state.access = access),
+    SET_CURRENT_USER: (state, user) => (state.currentUser = user),
+    SET_AUTH_ERROR: (state, error) => (state.authError = error),
+    CHANGE_DATA(state, data) {
+      if (state.userType === "student") {
         for (let key in data) {
-          state.studentInfo[key] = data[key]
+          state.studentInfo[key] = data[key];
         }
       } else {
-          for (let key in data) {
-            state.teacherInfo[key] = data[key]
-          }
+        for (let key in data) {
+          state.teacherInfo[key] = data[key];
         }
-      },
-    SET_USER_TYPE: (state, userType) => state.userType = userType,
+      }
     },
+    SET_USER_TYPE: (state, userType) => (state.userType = userType),
+  },
   actions: {
-    saveToken({commit}, access) {
-      commit('SET_TOKEN', access)
-      localStorage.setItem('access', access)
+    saveToken({ commit }, access) {
+      commit("SET_TOKEN", access);
+      localStorage.setItem("access", access);
     },
-    removeToken({commit}) {
-      commit('SET_TOKEN', '')
-      localStorage.setItem('access', '')
+    removeToken({ commit }) {
+      commit("SET_TOKEN", "");
+      localStorage.setItem("access", "");
     },
     login({ commit, dispatch }, credentials) {
       // 로그인 함수 구현
       axios({
         url: drf.accounts.login(),
-        method: 'post',
-        data: credentials
+        method: "post",
+        data: credentials,
       })
-        .then(res => {
-          const access = res.data.access
-          dispatch('saveToken', access)
-          commit('SET_CURRENT_USER', res.data) 
-          router.push({ name: 'educolab'})
-        })         
-        .catch(err => {
-          commit('SET_AUTH_ERROR', err.response.data)
+        .then((res) => {
+          const access = res.data.access;
+          dispatch("saveToken", access);
+          commit("SET_CURRENT_USER", res.data);
+          router.push({ name: "educolab" });
         })
+        .catch((err) => {
+          commit("SET_AUTH_ERROR", err.response.data);
+        });
     },
     signup(state) {
-      if (state.getters.getUserType == 'student') {
-        axios.post(drf.accounts.signup(), state.state.studentInfo)
+      if (state.getters.getUserType == "student") {
+        axios
+          .post(drf.accounts.signup(), state.state.studentInfo)
           .then(() => {
-            window.alert('회원가입이 완료되었습니다')
-            router.push({ name: 'login'})
+            window.alert("회원가입이 완료되었습니다");
+            router.push({ name: "login" });
           })
           .catch(() => {
-            window.alert('필수 항목이 빠져 있거나, 올바르지 않습니다')
-          })
-          
-        } else if (state.getters.getUserType == 'teacher') {
-          axios.post(drf.accounts.signup(), state.state.teacherInfo)
+            window.alert("필수 항목이 빠져 있거나, 올바르지 않습니다");
+          });
+      } else if (state.getters.getUserType == "teacher") {
+        axios
+          .post(drf.accounts.signup(), state.state.teacherInfo)
           .then(() => {
-            window.alert('회원가입이 완료되었습니다')
-            router.push({ name: 'login'})
+            window.alert("회원가입이 완료되었습니다");
+            router.push({ name: "login" });
           })
           .catch(() => {
-            window.alert('필수 항목이 빠져 있거나, 올바르지 않습니다')
-          })
+            window.alert("필수 항목이 빠져 있거나, 올바르지 않습니다");
+          });
       }
     },
-    logout({dispatch}) {
-        dispatch('removeToken')
-        router.push({ name : 'login' })
-      .catch(err => {
-        console.log(err.respone)
-      })
+    logout({ dispatch }) {
+      dispatch("removeToken");
+      router.push({ name: "login" }).catch((err) => {
+        console.log(err.respone);
+      });
     },
-    setUserType({commit}, userType) {
+    setUserType({ commit }, userType) {
       // 로그인할 때
       // 회원가입 페이지
-      commit('SET_USER_TYPE', userType)
+      commit("SET_USER_TYPE", userType);
     },
-    changeData({commit}, data) {
-      commit('CHANGE_DATA', data)
+    changeData({ commit }, data) {
+      commit("CHANGE_DATA", data);
     },
   },
-}
+};
