13d92fd28076e18d31640c392dab2a805bbe7aeb
홍찬기 <sal0203@naver.com>
2022-08-04 01:21:40 +0900

    feat: 과제 메인페이지 기능 어느정도 구현 완료 나머지는 상의필요

--- backend/educolab/settings.py
+++ backend/educolab/settings.py
@@ -60,11 +60,10 @@ environ.Env.read_env(
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
 
 ALLOWED_HOSTS = []
 
