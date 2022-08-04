474e20b7593ef2923763068b0a756f52de2f96d3
wonkyoung <pubhan35@gmail.com>
2022-08-04 22:32:22 +0900

    Fix : SurveyItem 컴포넌트화

---
+++ front-end/src/components/SurveyItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <select name="" id="" @change="selectedSurvey" v-model="selected">
+      <option value="">==문항 선택==</option>
+      <option value="choice">객관식 설문조사</option>
+      <option value="question">주관식 설문조사</option>
+    </select>
+
+    <br>
+
+    <div v-if="selected === 'question'">
+      <input type="text">주관식 문제
+    </div>
+
+    <div v-else-if="selected === 'choice'">
+      <div class="q-pa-md">
+        <div class="q-gutter-sm" disabled>
+          <q-radio v-model="shape" val="0" label="매우 좋음" />
+          <q-radio v-model="shape" val="1" label="좋음" />
+          <q-radio v-model="shape" val="2" label="중간" />
+          <q-radio v-model="shape" val="3" label="나쁨" />
+          <q-radio v-model="shape" val="4" label="매우 나쁨" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  name: 'SurveyItem',
+  props: {
+    survey: Number,
+  },
+  setup () {
+    return {
+      shape: ref(''),
+      selected: ref('')
+    }
+  },
+}
+</script>
--- front-end/src/views/SurveyCreateView.vue
+++ front-end/src/views/SurveyCreateView.vue
@@ -4,35 +4,7 @@
     <hr>
     <form action="">
       <div v-for="survey in surveyList" :key="survey">
-        <select name="" id="" @change="selectedSurvey" v-model="selected">
-          <option value="">==문항 선택==</option>
-          <option value="choice">객관식 설문조사</option>
-          <option value="question">주관식 설문조사</option>
-        </select>
-
-        <select name="" id="" @change="selectedSurvey" v-model="selected">
-          <option value="">==문항 선택==</option>
-          <option value="choice">객관식 설문조사</option>
-          <option value="question">주관식 설문조사</option>
-        </select>
-
-        <br>
-
-        <div v-if="selected === 'question'">
-          <input type="text">주관식 문제
-        </div>
-
-        <div v-else-if="selected === 'choice'">
-          <div class="q-pa-md">
-            <div class="q-gutter-sm" disabled>
-              <q-radio v-model="shape" val="0" label="매우 좋음" />
-              <q-radio v-model="shape" val="1" label="좋음" />
-              <q-radio v-model="shape" val="2" label="중간" />
-              <q-radio v-model="shape" val="3" label="나쁨" />
-              <q-radio v-model="shape" val="4" label="매우 나쁨" />
-            </div>
-          </div>
-        </div>
+        <survey-item :survey="survey"/>
       </div>
 
     </form>
@@ -40,20 +12,15 @@
 </template>
 
 <script>
-import {ref} from 'vue'
+import SurveyItem from '../components/SurveyItem.vue'
 
 export default {
+  components: { SurveyItem },
   name: 'SurveyCreateView',
-  setup () {
-    return {
-      shape: ref('')
-    }
-  },
   data() {
     // const surveyList = this.choices + this.questions
     // const surveyTitle = this.surveyList.length
     return {
-      selected : '',
       surveyList : 1
       // questions: 1,
       // surveyList: [],
