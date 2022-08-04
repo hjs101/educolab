2121f8678a8e89f13657b447640307c824b418f8
h5282001 <h5282001@naver.com>
2022-08-04 15:12:18 +0900

    Fix : URL 충돌 해결

--- backend/educolab/urls.py
+++ backend/educolab/urls.py
@@ -47,5 +47,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('notice/', include('notice.urls')),
     path('homework/', include('homework.urls')),
+    path('survey/', include('survey.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
