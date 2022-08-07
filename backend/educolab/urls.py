d215d15168a217d7ae481c7794456479b77bb84b
h5282001 <h5282001@naver.com>
2022-08-07 19:52:08 +0900

    자동배포로 인한 Url 변경

--- backend/educolab/urls.py
+++ backend/educolab/urls.py
@@ -39,15 +39,15 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
-    path('accounts/registration/', include('dj_rest_auth.registration.urls')),
-    path('accounts/', include('accounts.urls')), 
-    path('admin/', admin.site.urls),
-    path('notice/', include('notice.urls')),
-    path('homework/', include('homework.urls')),
-    path('survey/', include('survey.urls')),
-    path('mypage/', include('mypage.urls')),
+    path(r'api/swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'api/swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'api/redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
+    path('api/accounts/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/accounts/', include('accounts.urls')), 
+    path('api/admin/', admin.site.urls),
+    path('api/notice/', include('notice.urls')),
+    path('api/homework/', include('homework.urls')),
+    path('api/survey/', include('survey.urls')),
+    path('api/mypage/', include('mypage.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
