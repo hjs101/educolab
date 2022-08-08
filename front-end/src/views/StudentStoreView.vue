be5e24183397c62712d33a3764d2cf1ac8c0643a
여동준 <tgc03042@naver.com>
2022-08-09 01:51:35 +0900

    nyam

--- front-end/src/views/StudentStoreView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>포인트 페이지</h1>
-  <router-view />
-  </div>
-</template>
--- front-end/src/views/StudentWritingView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>학생 필기 페이지</h1>
-  <router-view />
-  </div>
-</template>
