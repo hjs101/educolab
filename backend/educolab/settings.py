232c743e208a5ed22be5e86e82a4733978a8ada7
hjs101 <gkswlstjdwkd@naver.com>
2022-08-16 10:56:56 +0900

    Fix : 화이트리스트 추가

--- backend/educolab/settings.py
+++ backend/educolab/settings.py
@@ -404,7 +404,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://192.168.0.15:8080",
     "https://i7c102.p.ssafy.io",
-    "118.222.223.154"
+    "http://118.222.223.154"
 ]
 =======
 CORS_ALLOW_ORIGINS_ALL = True
