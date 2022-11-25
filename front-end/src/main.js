cff56e125b18f66ddf1ff12df3329ffd3d8e276d
hjs101 <gkswlstjdwkd@naver.com>
2022-11-25 16:38:48 +0900

    edit vue settings

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,7 +1,10 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import './plugins/axios'
 >>>>>>> 1f63946 (vue 환경셋팅)
+=======
+>>>>>>> b1d70e4 (edit vue settings)
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
