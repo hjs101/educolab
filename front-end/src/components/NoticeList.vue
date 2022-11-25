3949627d36d77b187df14c0e26f4de23ae9ffd0f
hjs101 <gkswlstjdwkd@naver.com>
2022-11-25 16:39:49 +0900

    front branch에 merge
    Style : #90

--- front-end/src/components/NoticeList.vue
+++ front-end/src/components/NoticeList.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>공지사항 리스트</h1>
+<<<<<<< HEAD
     <table>
       <thead>
         <tr>
@@ -8,6 +9,8 @@
         </tr>
       </thead>
     </table>
+=======
+>>>>>>> 5e670de (front branch에 merge)
   </div>
 </template>
 
